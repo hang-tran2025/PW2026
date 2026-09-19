2164fcb7a7886a58494127a61b38e67b492f6099
Hang Tran <ltrc.hang.prod@gmail.com>
2026-09-19 15:33:56 +0700

    Update drowndown

--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -1,7 +1,14 @@
 import {test, expect} from '@playwright/test';
-test('verify dropdown success', async ({page}) => {
+test('verify select a dropdown success', async ({page}) => {
     await page.goto('https://the-internet.herokuapp.com/dropdown', {waitUntil: 'domcontentloaded'});
     await page.getByRole('combobox').selectOption('1');
     await expect(page.getByRole('combobox')).toHaveValue('1'); 
     
+})
+
+test('verify select multiple dropdown success', async ({page}) => { 
+    await page.goto('https://qa-demo-site-ten.vercel.app/elements/dropdown', {waitUntil: 'domcontentloaded'});
+
+    await page.getByRole('listbox').selectOption([{label: 'TypeScript'}, {label: 'Python'}]);
+    await expect(page.getByRole('listbox').locator('option:checked')).toHaveText(['TypeScript', 'Python']);
 })
